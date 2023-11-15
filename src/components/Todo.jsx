293236baeb43047454eebbdaac17bcc5cfe33bf1
VishalPachpor <Vishalpatil080502@gmail.com>
2023-11-15 14:51:17 +0530

    TODO added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { UseSelector, useSelector } from 'react-redux'
+import {removeTodo} from "../features/todo/todoSlice"
 
 function Todo() {
+  const todos = useSelector((state)=>state.todos)
+
   return (
     <div>Todo</div>
   )
