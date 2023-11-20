0d601312d6bb2fc38b69af0d0030b4421bb123c5
VishalPachpor <Vishalpatil080502@gmail.com>
2023-11-20 17:38:28 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
-
+import AddTodo from "./components/AddTodo"
 import './App.css'
+import Todo from './components/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1>This code is for react redux</h1>
+      <AddTodo />
+      <Todo />
     </>
   )
 }
